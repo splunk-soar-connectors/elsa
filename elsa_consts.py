d8c7a0445346df51b57dc07b969036922634c1cc
splunk-soar-connectors-admin <admin@splunksoar>
2021-12-02 09:22:31 -0800

    Updating source files with legal boilerplate

--- elsa_consts.py
+++ elsa_consts.py
@@ -1,17 +1,20 @@
-# --
 # File: elsa_consts.py
 #
-# Copyright (c) Phantom Cyber Corporation, 2018
+# Copyright (c) 2018 Splunk Inc.
 #
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+#
+#
 # of Phantom Cyber Corporation.
-#
-# --
-
 ELSA_BASE_URL = "base_url"
 ELSA_QUERY_URL = "%s/elsa-query/API/query"
 TEST_QUERY = "qryGetSelectFields?type=EVENT&groupType=NO_GROUP"
